61ec66aaf961888c0565b007361351b819987551
twsho <twsho1996@gmail.com>
2025-07-25 14:54:32 +0900

    page公開のための修正

--- rei-tw-realestate/vite.config.ts
+++ rei-tw-realestate/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rei-tw-realestate/', // GitHubリポジトリ名に合わせて設定
+  base: '/Rei/', // GitHubリポジトリ名に合わせて修正
   plugins: [react()],
 })
